505760bce7e0b2d903730597694408c3dc2df79a
Achraf Boutzad <achrafboutzad@gmail.com>
2024-12-07 03:56:50 +0100

    add some code in the controller

--- src/main/java/com/E_COM_App/E_COM_App/Controller/ProductController.java
+++ src/main/java/com/E_COM_App/E_COM_App/Controller/ProductController.java
@@ -1,10 +1,18 @@
 package com.E_COM_App.E_COM_App.Controller;
 
 
+import com.E_COM_App.E_COM_App.Service.InterProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+    @Autowired
+    InterProductService interProductService;
+
+
+
+
 
 
 
