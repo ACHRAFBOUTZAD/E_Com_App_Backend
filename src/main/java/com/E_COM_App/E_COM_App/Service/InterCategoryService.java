3986b833cd3f87087935363199fa52402c2e5490
Achraf Boutzad <achrafboutzad@gmail.com>
2024-12-09 18:59:48 +0100

    add Category Service

--- src/main/java/com/E_COM_App/E_COM_App/Service/InterCategoryService.java
+++ src/main/java/com/E_COM_App/E_COM_App/Service/InterCategoryService.java
@@ -5,8 +5,12 @@ import com.E_COM_App.E_COM_App.model.Category;
 import java.util.List;
 
 public interface InterCategoryService {
+    Category addCategory(Category category);
+    Category removeCategoryById(Long category_id);
+    Category updateCategory(Category category,Long category_id);
+    Category getCategoriesById(Long category_id);
+    Category getCategoriesByName(String categoryName);
     List<Category> getAllCategories();
-    List<Category> getCategoriesById(Long categoryId);
-    List<Category> getCategoriesByName(String categoryName);
+
 
 }
